956e9a80031b0aa2e6345044005cab4be28a8d7e
Adhiksit23 <kalra3@purdue.edu>
2026-09-21 23:45:03 +0530

    Missed Last july date when making update commands

--- services/monthly_update_shot.py
+++ services/monthly_update_shot.py
@@ -1307,6 +1307,6 @@ if __name__ == "__main__":
     #     month=6,
     # )
     update_date_range(
-        start_date="2026-09-01",
-        end_date="2026-09-09",
+        start_date="2026-07-30",
+        end_date="2026-08-01",
     )
